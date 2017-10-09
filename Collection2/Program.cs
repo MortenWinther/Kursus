2694c04fe7837ed14e7921207ba07b6c75e714a4
MortenWinther <mortenwinther@mail.dk>
2017-10-09 15:52:56 +0200

    Slut dag 5

---
+++ Collection2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bunke b = new Bunke();
+
+
+
+        }
+    }
+    public class Kort {
+        public string Kulør { set; get; }
+        public int Værdi { set; get; }
+        public override string ToString()               
+            {
+                return this.Kulør + " " + this.Værdi;
+            }
+
+    }
+    public class Bunke
+    {
+        private Stack<Kort> bunke = new Stack<Kort>();
+        public void TilføjKort(Kort k)
+        {
+            bunke.Push(k);
+        }
+    }
+
+ }
+
---
+++ LogDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using NLog;
+using System;
+namespace LogDemo
+{
+    class Program
+    {
+        private static Logger logger=LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            logger.Trace("Enter");
+            logger.Debug("Kald test1");
+            test1(5, 5);
+            logger.Debug("Retur test1");
+            logger.Debug("Kald test2");
+            test2();
+            logger.Debug("Retur test1");
+        }
+            static void test1(int i, int x)
+        {
+            Console.WriteLine("test1");
+            System.Threading.Thread.Sleep(1000);
+        }
+
+        static void test2()
+        {
+            try
+            {
+                Console.WriteLine("test2");
+                throw new ApplicationException("fejl");
+            }
+            catch (Exception ex)
+            {
+                // mangler log
+               
+            
+
+            }
+        }
+    }
+}
+
