9c500b23fc112bb8e4d5a515b61649e1d08ee627
MortenWinther <mortenwinther@mail.dk>
2017-10-10 16:08:51 +0200

    Slut dag 6

---
+++ Delegates2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates2
+{
+    class Program
+    {       
+        static void Main(string[] args)
+        {
+            Maskine m = new Maskine();
+            m.Log = Console.WriteLine;
+            m.Log += AppendLog;
+
+            // ekstra:
+            // m.log+= delegate (string txt) { System.IO.File.AppendAllText(@"c:\temp\log.txt", txt + "\r\n");};
+            // m.log += (txt) => { System.IO.File.AppendAllText(@"c:\temp\log.txt", txt + "\r\n");};
+
+            m.Start();
+            m.Stop();
+
+
+        }
+
+        static void AppendLog(string txt)
+        {
+            System.IO.File.AppendAllText(@"c:\temp\log.txt", txt + "\r\n");
+        }
+    }
+
+    //public delegate void LogDelegate(string txt); //1
+
+    public class Maskine
+    {
+        public Action<string> Log{ get; set; } //Bruges i stedet for //1
+        //public LogDelegate Log { get; set; } //1
+
+        public void Start()
+        {
+            Log(DateTime.Now.ToLongDateString() + " - " + DateTime.Now.ToLongTimeString() + ": " + "Starter");
+        }
+
+        public void Stop()
+        {
+            Log(DateTime.Now.ToLongDateString() + " - " + DateTime.Now.ToLongTimeString()+": " + "Stopper");
+        }
+    }
+}
---
+++ FileWatch/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.FileSystemWatcher w =
+                new System.IO.FileSystemWatcher(@"c:\temp", "*");
+            w.EnableRaisingEvents = true;
+            w.NotifyFilter = System.IO.NotifyFilters.FileName | System.IO.NotifyFilters.Size | System.IO.NotifyFilters.DirectoryName;
+
+            w.Changed += W_Changed;
+            //do { } while (true);
+            Console.ReadKey();
+
+        }
+
+        private static void W_Changed(object sender, System.IO.FileSystemEventArgs e)
+        {
+            Console.WriteLine(e.Name + " User: " + Environment.UserName);
+        }
+    }
+}
